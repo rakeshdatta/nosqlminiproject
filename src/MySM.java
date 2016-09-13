46d4bed4818dbfd8c642586e889b74fa9b62138b
Rakesh Datta <rakesh.datta6@gmail.com>
2016-09-13 00:19:05 -0700

    Adding comments

--- src/MySM.java
+++ src/MySM.java
@@ -1,107 +1,51 @@
-
+/********************************************************
+ * AUTHOR      : Rakesh Datta                           *
+ * COURSE      : CMPE 281 Mini Project                  *
+ * DATE        : April 2016                             *
+ * DESCRIPTION : This Class Implements the interface SM.*
+ *               SM Interface contains methods for the  *
+ *               Database.                              *
+ * *****************************************************/
+ 
 import java.util.*;
 import java.io.*;
 
-/**
- *  Description of the Class
- *
- *@author     Paul Nguyen
- *@created    March 18, 2003
- */
 public class MySM implements SM {
 
 	private SM sm;
 
-/*
-        public SM.OID getOID( byte[] oidbytes ) {
-	  return null ;
-        }
-*/
 
-	/**
-	 *  Constructor for the MySM object
-	 *
-	 *@since
-	 */
 	public MySM() {
 		this.sm = SMFactory.getInstance();
 	}
 
 
-
-	/**
-	 *  Description of the Method
-	 *
-	 *@param  rec              Description of Parameter
-	 *@return                  Description of the Returned Value
-	 *@exception  IOException  Description of Exception
-	 *@since
-	 */
 	public SM.OID store(Record rec) throws IOException {
 		return sm.store(rec);
 	}
 
 
-	/**
-	 *  Description of the Method
-	 *
-	 *@param  oid                    Description of Parameter
-	 *@return                        Description of the Returned Value
-	 *@exception  NotFoundException  Description of Exception
-	 *@exception  IOException        Description of Exception
-	 *@since
-	 */
 	public Record fetch(SM.OID oid) throws NotFoundException, IOException {
 		return sm.fetch(oid);
 	}
 
 
-	/**
-	 *  Description of the Method
-	 *
-	 *@exception  SM.IOException  Description of Exception
-	 *@since
-	 */
 	public void close() throws SM.IOException {
 		sm.close();
 	}
 
 
-	/**
-	 *  Description of the Method
-	 *
-	 *@exception  SM.IOException  Description of Exception
-	 *@since
-	 */
 	public void flush()  {
 		try { sm.flush(); } catch (Exception e) {}
 	}
 
 
 
-	/**
-	 *  Description of the Method
-	 *
-	 *@param  oid                    Description of Parameter
-	 *@param  rec                    Description of Parameter
-	 *@return                        Description of the Returned Value
-	 *@exception  NotFoundException  Description of Exception
-	 *@exception  IOException        Description of Exception
-	 *@since
-	 */
 	public SM.OID update(SM.OID oid, Record rec) throws NotFoundException, IOException {
 		return sm.update(oid, rec);
 	}
 
 
-	/**
-	 *  Description of the Method
-	 *
-	 *@param  oid                        Description of Parameter
-	 *@exception  NotFoundException      Description of Exception
-	 *@exception  CannotDeleteException  Description of Exception
-	 *@since
-	 */
 	public void delete(SM.OID oid) throws NotFoundException, CannotDeleteException {
 		sm.delete(oid);
 	}
@@ -110,19 +54,22 @@ public class MySM implements SM {
         	return sm.getOID( oidbytes ) ;	  
         } 
 
+        /*
+         * This an inner class used to create a Hashmap. This hashmap
+         * is used for (oid, primary key) mapping. 
+         * Given the primary key this Map gives oid. The oid is then used
+         * to fetch the entire record from SM.
+         *
 
 	public class MyHashMap {
-    		private Map<String, String> map = new HashMap<String, String>();
+    		private Map<String, String> map  = new HashMap<String, String>();
     		private Map<String, String> map1 = new HashMap<String, String>();
 
     		MyHashMap(){
-
+                   System.out.println("Hashmap Created");
     		};
 
     		public void create(SM.OID oid, String key){
-        	   //System.out.println("guid: "+oid.toBytes());
-        	   //String guid = new String (oid.getKey());
-
         	   String guid = new String (oid.toBytes());
         	   System.out.println("[INFO]: map_create:key/guid  = "+guid);
         	   System.out.println("[INFO]: map_create:value     = "+key);
